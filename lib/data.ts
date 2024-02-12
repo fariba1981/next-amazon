12fa067c30cb7fbec1886d008a351cf2e16d258b
Fariba Mohammadi <fariba.mohammadi1981@gmial.com>
2024-02-12 20:28:14 +0300

    latest-products

---
+++ lib/data.ts
@@ -0,0 +1,84 @@
+const data = {
+    products: [
+        {
+            name: "Free Shirt",
+            slug: "free-shirt",
+            image: "/images/shirt1.jpg",
+            banner: "/images/banner1.jpg",
+            price: 70,
+            brand: "Nike",
+            description: "a popular shirt",
+            category: "Shirts",
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 20,
+            isFeatured: true
+        },
+        {
+            name: "Fit Shirt",
+            slug: "fit-shirt",
+            image: "/images/shirt2.jpg",
+            banner: "/images/banner2.jpg",
+            price: 80,
+            brand: "Adidas",
+            description: "a popular shirt",
+            category: "Shirts",
+            rating: 3.5,
+            numReviews: 10,
+            countInStock: 20,
+            isFeatured: true
+        },
+        {
+            name: "Slim Shirt",
+            slug: "slim-shirt",
+            image: "/images/shirt3.jpg",
+            banner: "/images/banner3.jpg",
+            price: 90,
+            brand: "Rymond",
+            description: "a popular shirt",
+            category: "Shirts",
+            rating: 4.5,
+            numReviews: 3,
+            countInStock: 20,
+            isFeatured: true
+        },
+        {
+            name: "Golf Pants",
+            slug: "golf-pants",
+            image: "/images/pants1.jpg",
+            price: 90,
+            brand: "Oliver",
+            description: "smart looking pants",
+            category: "Pants",
+            rating: 2.9,
+            numReviews: 13,
+            countInStock: 20,
+        },
+        {
+            name: "Fit Pants",
+            slug: "fit-pants",
+            image: "/images/pants2.jpg",
+            price: 95,
+            brand: "Zara",
+            description: "smart looking pants",
+            category: "Pants",
+            rating: 3.8,
+            numReviews: 7,
+            countInStock: 20,
+        },
+        {
+            name: "Classic Pants",
+            slug: "classic-pants",
+            image: "/images/pants3.jpg",
+            price: 75,
+            brand: "Casely",
+            description: "smart looking pants",
+            category: "Pants",
+            rating: 2.4,
+            numReviews: 14,
+            countInStock: 20,
+        }
+        
+    ]
+}
+export default data;
